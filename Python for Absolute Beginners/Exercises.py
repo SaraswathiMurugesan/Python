aac35cd12e8bd8e6a38fd302d145873e55bf39eb
SaraswathiMurugesan <54387083+SaraswathiMurugesan@users.noreply.github.com>
2023-01-22 23:05:32 +0530

    Update Exercises.py

--- Python for Absolute Beginners/Exercises.py
+++ Python for Absolute Beginners/Exercises.py
@@ -1,1 +1,16 @@
 
+#Variables and assisgnment
+
+integer = 7
+float = 32.5
+bool = true
+integer = 10
+
+# comments and Math Operator
+summed = 2+3
+difference = 7 - 4
+divided = 1000/10
+product = 7*5
+exponent = 3**3
+floored = 10 // 3
+mod = 17 % 15
