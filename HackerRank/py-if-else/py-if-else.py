51e0d20859a651b56f0db55d613c679b84b9d03f
SaraswathiMurugesan <54387083+SaraswathiMurugesan@users.noreply.github.com>
2023-03-11 23:50:07 +0530

    Update py-if-else.py

--- HackerRank/py-if-else/py-if-else.py
+++ HackerRank/py-if-else/py-if-else.py
@@ -6,13 +6,12 @@ import random
 import re
 import sys
 
-if __name__ == '__main__':
-    n = int(input().strip())
-    if ((n % 2) == 0) and (n==range(2, 5)):
-        print('Not Weird')
-    elif((n % 2) == 0) and (n==range(6, 20)):
-        print('Not weird')
-    elif((n % 2) == 0) and (n>20):
-        print('Not Weird')
-    else:
-        print('Weird')
+n = int(input('Enter n: ').strip())
+if n % 2 == 1:
+    print('Weird')
+elif ((n % 2) == 0) and 2 <= n <= 5:
+    print('Not Weird')
+elif((n % 2) == 0) and 6 <= n <= 20:
+    print('Weird')
+else:
+    print('Not Weird')
