49a30f617a180636de83251a2f1dd850fb4355d2
SaraswathiMurugesan <54387083+SaraswathiMurugesan@users.noreply.github.com>
2024-03-30 00:36:10 +0530

    Create Day1_Exercise.py

---
+++ Day1_Exercise.py
@@ -0,0 +1,35 @@
+#Exercise 1
+#Write a Python program following the steps below:
+#1. Create a string and associate it with a variable named my_variable.
+#2. Print out the type of the variable.
+************************************
+#Program:
+my_variable = 10
+print(type[my_variable]);
+#Output:
+<class 'str'>
+*************************************
+#Exercise 2
+#Write a Python program that gets input from the user and prints out that input.
+*************************************
+#Program:
+number = input('Enter a number:')
+print('Number Entered:', number);
+#Output:
+Enter a number:10
+Number Entered: 10
+*************************************
+#Exercise 3
+#Create a list of four strings and associate the list with a variable named my_variable.
+**************************************
+#Program:
+my_variable = ['Python', 'Swift', 'C++','Java']
+print('my_variable list:',my_variable)
+#Output:
+my_variable list: ['Python', 'Swift', 'C++', 'Java']
+***************************************
+
+
+
+
+
