ba5df902fc490ffb0f51c62694cdbbb9287bd650
SaraswathiMurugesan <54387083+SaraswathiMurugesan@users.noreply.github.com>
2024-03-31 02:06:12 +0530

    Create Day2.py

---
+++ Day2.py
@@ -0,0 +1,9 @@
+#Assign some values to variables x, y, and z. Then, insert x, y, and z inside a print() function to print out the values of those variables.
+*******************************************
+#Program
+x = 2
+y = 3
+z = 4
+print(x,y,z)
+#Output
+2 3 4
